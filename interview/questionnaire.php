4c68009de51619728134d1155b61a3fe45a586c5
Brandon Robison <me@brandonrobison.com>
2015-12-05 10:59:58 -0600

    Moved interview forms into their own directory

--- interview/questionnaire.php
+++ interview/questionnaire.php
@@ -1,7 +1,7 @@
 <?php namespace Timecapsl;
 
-require __DIR__ . '/vendor/autoload.php';
-require_once __DIR__ . '/config.php';
+require '../vendor/autoload.php';
+require_once '../config.php';
 
 // questionnaire.php
 // Sends an email from the site new client / pre interview questionnaire to the site admin
--- interview/storyteller.php
+++ interview/storyteller.php
@@ -1,7 +1,7 @@
 <?php namespace Timecapsl;
 
-require __DIR__ . '/vendor/autoload.php';
-require_once __DIR__ . '/config.php';
+require '../vendor/autoload.php';
+require_once '../config.php';
 
 // storyteller.php
 // Sends an email from the site storyteller form to the site admin
